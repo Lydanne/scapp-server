9145c29cead44188f7de188b9b61e2a9fb25a023
Lydanne <1829913225@qq.com>
2024-11-05 00:27:27 +0800

    feat: 添加 auth 模块

--- apps/app/src/app/mod.rs
+++ apps/app/src/app/mod.rs
@@ -6,6 +6,7 @@ pub mod dto;
 pub mod exception;
 pub mod service;
 
+// use crate::modules::auth::AuthModule;
 use crate::modules::user::UserModule;
 use controller::AppController;
 use service::AppService;
